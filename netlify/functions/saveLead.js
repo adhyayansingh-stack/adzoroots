ce0e981c205af5544b70b614a52c12246f3bb7b6
Adhyayan Singh <adhyayansingh@gmail.com>
2026-01-25 01:25:32 +0530

    Fix Netlify function and config

--- netlify/functions/saveLead.js
+++ netlify/functions/saveLead.js
@@ -7,26 +7,12 @@ const pool = new Pool({
 });
 
 export const handler = async (event) => {
-  if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 405,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ message: "Method not allowed" })
-    };
-  }
-
   try {
     const data = JSON.parse(event.body);
 
     await pool.query(
       "INSERT INTO leads(name,email,phone,company,message) VALUES($1,$2,$3,$4,$5)",
-      [
-        data.name,
-        data.email,
-        data.phone,
-        data.company,
-        data.message
-      ]
+      [data.name, data.email, data.phone, data.company, data.message]
     );
 
     return {
@@ -35,8 +21,7 @@ export const handler = async (event) => {
       body: JSON.stringify({ message: "Thank you! We’ll contact you soon." })
     };
   } catch (err) {
-    console.error("DB Error:", err);
-
+    console.error("Function error:", err);
     return {
       statusCode: 500,
       headers: { "Content-Type": "application/json" },
